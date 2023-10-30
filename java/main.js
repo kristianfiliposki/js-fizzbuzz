700d464b94f99b38179285005595debcb86105ff
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-10-30 20:54:32 +0100

    1.0

---
+++ java/main.js
@@ -0,0 +1,14 @@
+
+
+for (let i = 0; i < 100; i++) {
+    console.log(i)
+    if(i % 15==0){
+        console.log("divisibile diviso entrambi");
+    }
+    else if(i % 3 ==0) {
+        console.log("è divisibile diviso 3");
+    }   
+    else if(i % 5 ==0){
+        console.log("divisibile diviso 5");
+    }
+}
