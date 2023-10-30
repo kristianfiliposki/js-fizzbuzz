ba577edbbc651c0ea41d2021f0387fa5783098a7
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-10-30 23:24:21 +0100

    1.1

--- java/main.js
+++ java/main.js
@@ -1,9 +1,15 @@
 
-
 for (let i = 0; i < 100; i++) {
     console.log(i)
+    const node = document.createElement("li");
+
     if(i % 15==0){
-        console.log("divisibile diviso entrambi");
+    console.log("divisibile diviso entrambi");
+    let node = document.createElement("li");
+    const textnode = document.createTextNode("MarinaPlus");
+    node.appendChild(textnode);
+    document.getElementById("myList").append(node);
+
     }
     else if(i % 3 ==0) {
         console.log("è divisibile diviso 3");
@@ -12,3 +18,4 @@ for (let i = 0; i < 100; i++) {
         console.log("divisibile diviso 5");
     }
 }
+
