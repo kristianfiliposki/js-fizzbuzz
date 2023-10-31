43c1d2c665b1db23f5cd47c0dade77d6f5e7903c
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-10-31 12:54:38 +0100

    1.2

--- java/main.js
+++ java/main.js
@@ -1,21 +1,43 @@
+const container=document.getElementById("myList");
 
 for (let i = 0; i < 100; i++) {
     console.log(i)
-    const node = document.createElement("li");
-
+    let node = document.createElement("div");
+    
+    
     if(i % 15==0){
-    console.log("divisibile diviso entrambi");
-    let node = document.createElement("li");
-    const textnode = document.createTextNode("MarinaPlus");
-    node.appendChild(textnode);
-    document.getElementById("myList").append(node);
-
+        console.log("divisibile diviso entrambi");
+        node.innerHTML+="FIZZ"
+        node.style.backgroundColor="blue";
+        
+        
+        
     }
     else if(i % 3 ==0) {
         console.log("è divisibile diviso 3");
+        node.innerHTML+="BUZZ"
+        node.style.backgroundColor="orange";
+        
+        
     }   
     else if(i % 5 ==0){
         console.log("divisibile diviso 5");
+        node.innerHTML+="FIZZBUZZ"
+        node.style.backgroundColor="aqua";
+        node.style.FontSize="2px";
+        
+        
+        
+        
     }
+    else{
+        node.innerHTML+=i
+        node.style.backgroundColor="white";
+        
+    }
+    
+    container.append(node);
+
+
 }
 
